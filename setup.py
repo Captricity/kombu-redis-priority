048703f8d847aa4b3138d3d9a8ead5d63ee14c09
Shaon Barman <shaonb@captricity.com>
2017-01-26 14:47:11 -0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kombu-redis-priority',
+    packages=find_packages(),
+    version='0.0.1',
+    description='Celery backend using redis SortedSets for priority',
+    include_package_data=True,
+    author='Captricity',
+    author_email='webmaster@captricity.com',
+    url='https://github.com/Captricity/kombu-redis-priority',
+    download_url = 'https://github.com/Captricity/kombu-redis-priority/tarball/0.0.1',
+    keywords = ['redis', 'sorted-set', 'kombu'],
+    classifiers = [],
+    install_requires=open('requirements.txt').read()
+)
