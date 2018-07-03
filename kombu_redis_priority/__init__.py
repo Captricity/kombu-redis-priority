d9ae4d64d1ed0304d187df642c29e9636944af61
Yoriyasu Yano <yorinasub17@users.noreply.github.com>
2018-07-02 17:03:37 -0700

    Bump version

--- kombu_redis_priority/__init__.py
+++ kombu_redis_priority/__init__.py
@@ -1,1 +1,1 @@
-VERSION = __version__ = "0.2.0"
+VERSION = __version__ = "0.3.0"
--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name='kombu-redis-priority',
     packages=find_packages(),
-    version='0.2.0',
+    version='0.3.0',
     description='Celery backend using redis SortedSets for priority',
     include_package_data=True,
     author='Captricity',
     author_email='webmaster@captricity.com',
     url='https://github.com/Captricity/kombu-redis-priority',
-    download_url='https://github.com/Captricity/kombu-redis-priority/tarball/0.2.0',
+    download_url='https://github.com/Captricity/kombu-redis-priority/tarball/0.3.0',
     keywords=['redis', 'sorted-set', 'kombu'],
     classifiers=[],
     install_requires=[
